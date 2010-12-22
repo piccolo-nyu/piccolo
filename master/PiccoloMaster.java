1583ca4e3f1440549c63511402bc5cc3150096d1
yasemin <yavcular@gmail.com>
2010-12-22 16:26:38 -0500

    removed old files

--- master/PiccoloMaster.java
+++
@@ -1,9 +0,0 @@
-package edu.nyu.cs.piccolo.master;
-
-import java.io.IOException;
-import java.util.Hashtable;
-import edu.nyu.cs.piccolo.kernel.PiccoloTable;
-
-public abstract class PiccoloMaster {
-	
-}
