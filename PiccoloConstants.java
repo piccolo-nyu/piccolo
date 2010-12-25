bed1e98c612b6d5f66fbeb6d5cc59efbc8a73995
yasemin <yavcular@gmail.com>
2010-12-25 16:47:18 -0500

    bin-added

--- PiccoloConstants.java
+++
@@ -1,9 +0,0 @@
-package edu.nyu.cs.piccolo;
-
-public interface PiccoloConstants {
-	
-	public static int PiccoloWorkerServerHandlerCount = 3;
-	public static int PiccoloWorkerServerPort = 55055;
-	
-	
-}
